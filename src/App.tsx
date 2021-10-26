47cddf2818be6597806e9e3488fbd98e81c9ebe9
akiko.kano <47952174+ichirin-planet@users.noreply.github.com>
2021-10-26 20:02:43 +0900

    VFC

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./types/todo";
+import { Text } from "./Text";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -15,10 +16,11 @@ export default function App() {
   };
   return (
     <div className="App">
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
-          key={todo.key}
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
